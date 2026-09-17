dbe551a16c0bbb67660230523d79f9f0e4caa1d2
HashemKil <hashem-123123@outlook.com>
2026-09-17 16:34:36 +0300

    feat: add responsive grid layout and navigation bar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Grid, GridItem, Show } from "@chakra-ui/react"
 
 function App() {
   return <Grid templateAreas={{
-    base: '"nav" "main"'
+    base: '"nav" "main"',
     lg: '"nav nav" "aside main"'
   }}>
-    <GridItem area='nav' bg='coral'>
-      Nav
+    <GridItem area='nav'>
+      <NavBar />
     </GridItem>
     <Show above="lg">
       <GridItem area='aside' bg='coral'>

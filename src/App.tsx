156424546cf9dc4d89f08f3450eb327594fe76f4
HashemKil <hashem-123123@outlook.com>
2026-09-18 21:05:00 +0300

    Search Input

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery)
@@ -23,7 +24,7 @@ function App() {
     lg: '200px 1fr'
   }}>
     <GridItem area='nav'>
-      <NavBar />
+      <NavBar onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })} />
     </GridItem>
     <GridItem area='aside' paddingX={5}>
       <GenreList selectedGenre={gameQuery.genre} onSelectedGenre={(genre) => setGameQuery({ ...gameQuery, genre })} />

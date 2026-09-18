524fd5df46e1f79430d8bc1d94001f9854906f8f
HashemKil <hashem-123123@outlook.com>
2026-09-18 20:54:51 +0300

    Search Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack } from "@chakra-ui/react"
+import { Box, Flex, Grid, GridItem, HStack } from "@chakra-ui/react"
 import NavBar from "./components/ui/NavBar"
 import GameGrid from "./components/ui/GameGrid"
 import GenreList from "./components/GenreList"
@@ -29,10 +29,12 @@ function App() {
       <GenreList selectedGenre={gameQuery.genre} onSelectedGenre={(genre) => setGameQuery({ ...gameQuery, genre })} />
     </GridItem>
     <GridItem area='main'>
-      <HStack spacing={5} paddingLeft={2} marginBottom={5}>
-        <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
+      <Flex paddingLeft={2} marginBottom={5}>
+        <Box marginRight={5}>
+          <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
+        </Box>
         <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
-      </HStack>
+      </Flex>
       <GameGrid gameQuery={gameQuery} />
     </GridItem>
   </Grid>
